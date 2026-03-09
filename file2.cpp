d7919bfd0230736248bba7b2a25753bba1944316
raffibromabar-web <raffibromabar@gmail.com>
2026-03-09 16:40:41 +0700

    tambahkan rerata  = Rata_rata (nilBI,nilMT)

--- file2.cpp
+++ file2.cpp
@@ -13,4 +13,8 @@ int main(){
     cin >> nilBI;
     cout << "Masukan nilai Matematika = ";
     cin >> nilMT;
+
+    rerata = Rata_rata(nilBI,nilMT);
+
+    cout << "Status kelulusan = "<<Status_lulus(rerata)<<" ,dengan nilai rerata = "<<rerata<<endl;
 }
